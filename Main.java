3e5020b4ec0c15167914f9a9a02ea07b98df23ac
Kacper Ormaniec <kacperormaniec@gmail.com>
2024-05-05 14:07:26 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Person person = new Person("John", 30);
+            Person person = new Person("Kacper", 21);
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
         } catch (InvalidAgeException e) {
             System.out.println("Error: " + e.getMessage());
         }
+
+        
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("Wiadomosc email!");
     }
 }
