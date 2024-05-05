3cf3c3668bd08c0d5ae1f5f63b9f11a65b0fcc89
Kacper Ormaniec <kacperormaniec@gmail.com>
2024-05-05 14:24:09 +0200

    zad4

--- Main.java
+++ Main.java
@@ -15,12 +15,35 @@ public class Main {
 
         
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("Wiadomosc email!");*/
+        emailMessenger.sendMessage("Wiadomosc email!");
 
         int result = MathUtils.add(5, 7);
         System.out.println("Sum of 5 and 7 is: " + result);
 
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("The sum of 5 and 7 is: " + result);
+        emailMessenger.sendMessage("The sum of 5 and 7 is: " + result);*/
+
+        Person[] people = new Person[5];
+
+        final int b = 10;
+
+        try {
+            
+            people[0] = new Person("John Doe", 30);
+            people[1] = new Person("Kacper Orm", 25);
+            people[2] = new Person("Julia Orm", 40);
+            people[3] = new Person("Mikolaj S", 35);
+            people[4] = new Person("David willy", 28);
+
+  
+            for (Person person : people) {
+                int value = MathUtils.add(person.getAge(), b);
+
+                EmailMessenger emailMessenger = new EmailMessenger();
+                emailMessenger.sendMessage("Wartosc wyliczona dla " + person.getName() + " wynosi: " + value);
+            }
+        } catch (InvalidAgeException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
